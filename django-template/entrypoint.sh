9b0bf50c02e33aa3c36a95ae44f7bb44f82f87b5
tbalza <tomas.balza@gmail.com>
2024-08-13 16:57:32 +0200

    django. to do app

---
+++ django-template/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Run Django collectstatic
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
+
+# Apply database migrations
+echo "Applying database migrations..."
+python manage.py migrate
+
+# Start Gunicorn server
+echo "Starting Gunicorn server..."
+exec gunicorn myproject.wsgi:application --bind 0.0.0.0:8000
