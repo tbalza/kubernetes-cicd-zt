3fc76ada30a9f8c78bf0f5e9d3430d20a399e7c6
tbalza <tomas.balza@gmail.com>
2024-08-14 20:52:24 +0200

    django. to do app

--- argo-apps/jenkins/Jenkinsfile.groovy
+++ argo-apps/jenkins/Jenkinsfile.groovy
@@ -25,24 +25,7 @@ pipeline {
                 }
             }
         }
-        stage('Check Commit Author') {
-            steps {
-                container('jnlp') {
-                    script {
-                        env.GIT_AUTHOR = sh(script: "git show -s --format='%an' ${env.GIT_COMMIT}", returnStdout: true).trim()
-                        if (env.GIT_AUTHOR == 'argocd-image-updater') {
-                            echo "Commit by argocd-image-updater, skipping further actions."
-                            // Marking no build needed
-                            env.BUILD_NEEDED = 'false'
-                        }
-                    }
-                }
-            }
-        }
-        stage('Check ECR for Latest Image Commit') {
-            when {
-                expression { env.BUILD_NEEDED != 'false' }
-            }
+        stage('Check ECR for Latest Image Commit') { // pipeline-aws plugin outputs ecr images in a non-standard way, that needs to be filtered
             steps {
                 container('jnlp') {
                     script {
@@ -76,10 +59,7 @@ pipeline {
                 }
             }
         }
-        stage('Check for Django Changes') {
-            when {
-                expression { env.BUILD_NEEDED == 'true' }
-            }
+        stage('Check for Django Changes') { // check the diff of the the commit id (which is the name of image tag) vs. the current comment and check for changes in django
             steps {
                 container('jnlp') {
                     script {
